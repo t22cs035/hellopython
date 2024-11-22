6eae6151960ee0a8e204b454d784e1fd23ef7877
Shunya NAKAJIMA <t22cs035@yamanashi.ac.jp>
2024-11-22 16:30:40 +0900

    test

--- DjangoProject/newproject/myproject/shoppinglist/forms.py
+++ DjangoProject/newproject/myproject/shoppinglist/forms.py
@@ -1,4 +1,5 @@
 from django import forms
+from .models import Item
 
 class ItemBuy(forms.Form):
     status = (
@@ -7,3 +8,11 @@ class ItemBuy(forms.Form):
     )
     item_id = forms.IntegerField(label="ID")
     item_status = forms.ChoiceField(label="STATUS",widget=forms.Select,choices=status)
+    
+class ItemIdForm(forms.Form):
+    item_id = forms.IntegerField(label='ID')
+
+class ItemForm(forms.ModelForm):
+    class Meta:
+        model = Item
+        fields = ['name', 'item_url', 'count', 'buy_date', 'shop']
--- DjangoProject/newproject/myproject/shoppinglist/views.py
+++ DjangoProject/newproject/myproject/shoppinglist/views.py
@@ -3,6 +3,8 @@ from django.views.generic import ListView
 from .models import Item
 from django.http import HttpResponseRedirect
 from django.urls import reverse
+from .forms import ItemBuy, ItemIdForm, ItemForm
+from django.views.generic.base import TemplateView
 from django.shortcuts import get_object_or_404
 from .forms import ItemBuy
 
@@ -23,3 +25,19 @@ class ItemList(ListView):
         context = super().get_context_data(**kwargs)
         context["form"] = ItemBuy()
         return context
+    
+class ItemDeleteView(TemplateView):
+    model = Item
+    template_name = "shoppinglist/item_delete.html"
+    
+    def post(self,request,*args, **kwargs):
+        item_id = self.request.POST.get(item_id)
+        item = get_object_or_404(Item,pk=item_id)
+        item.delete()
+        return HttpResponseRedirect(reverse("shoppinglist:list"))
+    
+    def get_context_data(self, **kwargs) -> dict[str, Any]:
+        context = super().get_context_data(**kwargs)
+        context["form"] = ItemIdForm()
+        return context
+    
